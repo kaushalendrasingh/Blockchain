40c8e1707be5711dcc991c4a41f783819a88277d
kaushalendra singh <40608033+kaushalendrasingh@users.noreply.github.com>
2019-04-13 23:43:19 +0530

    Update test.js

--- test.js
+++ test.js
@@ -5,11 +5,12 @@ bitcoin.createNewBlock(234, '','YTG7G7896')
 
 
 bitcoin.createNewTransaction(23,'686F78JIB','786F7F8O');
-bitcoin.createNewTransaction(21,'512DE511D','2D23E123D');
-bitcoin.createNewTransaction(100,'3D23D22D','ASD123E123');
+bitcoin.createNewTransaction(21,'512DE511D','2D23E123D');// Transactions
 
-bitcoin.createNewBlock(1234, 'YTG7G7896','325F3GT54')
+bitcoin.createNewBlock(1234, 'YTG7G7896','325F3GT54')// Transaction added to this block
 bitcoin.createNewBlock(124, '325F3GT54','324F34F5')
 bitcoin.createNewBlock(1354, '324F34F5','32F235G32F')
+bitcoin.createNewTransaction(100,'3D23D22D','ASD123E123'); //Pending Transaction
+
 
 console.log(bitcoin.chain[1]);

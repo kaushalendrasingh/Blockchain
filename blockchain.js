cad89d42188578418953bd1d7ad699dd49888cc3
kaushalendra singh <40608033+kaushalendrasingh@users.noreply.github.com>
2019-04-14 13:09:36 +0530

    Update blockchain.js

--- blockchain.js
+++ blockchain.js
@@ -1,3 +1,5 @@
+const sha256 = require('sha256')
+
 function Blockchain () {
     this.chain = [];
     this.pendingTransactions = [];
@@ -31,5 +33,25 @@ Blockchain.prototype.createNewTransaction = function(amount, sender, recipient){
     return this.getLastBlock()['index'] +1
     
 }
+
+Blockchain.prototype.hashBlock = function(previousBlockHash, currentBlockData, nonce){
+    const dataAsString = previousBlockHash + nonce.toString() + JSON.stringify(currentBlockData);
+    const hash = sha256(dataAsString);
+    return hash;
+}
+
+Blockchain.prototype.proofOfWork = function(previousBlockHash , currentBlockData){
+    nonce =0;
+    let hash = this.hashBlock(previousBlockHash, currentBlockData, nonce)
+
+    while(hash.substring(0, 4) !== '0000'){
+        nonce++;
+        hash = this.hashBlock(previousBlockHash, currentBlockData, nonce)
+    }
+    console.log(hash);
+    
+    return nonce;
+}
+
 
 module.exports = Blockchain;
